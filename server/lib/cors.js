fc90ad9e2293b005d8cfef3d810900998b45bdea
Neil Cresswell @ Portainer <47971139+neil-cresswell-portainer@users.noreply.github.com>
2026-07-08 09:34:22 -0700

    Delete server directory

--- server/lib/cors.js
+++
@@ -1,5 +0,0 @@
-export const CORS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type,X-API-Key,Authorization,X-Portainer-URL',
-}
--- server/lib/http.js
+++
@@ -1,9 +0,0 @@
-/** @param {import('http').IncomingMessage} req */
-export function readBody(req) {
-  return new Promise((resolve, reject) => {
-    const chunks = []
-    req.on('data', (c) => chunks.push(c))
-    req.on('end', () => resolve(chunks.length ? Buffer.concat(chunks) : null))
-    req.on('error', reject)
-  })
-}
